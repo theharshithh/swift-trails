f9e38f4234b6362e39078aa925598bfd1c67e10e
harshith <theharshithdev@gmail.com>
2024-03-09 16:50:06 +0530

    env var

--- patient_manage.py
+++ patient_manage.py
@@ -1,13 +1,12 @@
 import os
-from dotenv import dotenv_values
+from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
 from io import BytesIO
 from openai import OpenAI
 
-secrets = dotenv_values('.env')
-
-openai_api_key = secrets["OPENAI_API_KEY"]
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
 
 if not openai_api_key:
     print("OPENAI_API_KEY env variable is not set.")
@@ -31,7 +30,7 @@ def get_patient_condition(medical_report):
     client = OpenAI(api_key=openai_api_key)
     instructions = """I want u to act like a simple flagging bot. Lets keep it simple. I will share with you a medical report or a patient's condition, and I need u to give me the following thing: True if the patient is fit for the medical trials, false if the patient is not fit for the medical trials.
     Here is a reference of Vital Data: Patient ID,Age,Sex,Temperature,Heart Beat,Respiratory Rate,Systolic BP,Diastolic BP,Oxygen Saturation\n1,29,M,98.6,72,16,120,80,98\n2,34,F,96.9,75,14,115,75,99\n3,45,M,98.4,70,18,110,70,97\n4,26,F,97.8,78,16,118,76,98\n5,37,M,97.1,72,15,120,80,96\n6,50,F,98.6,74,16,122,82,97\n7,32,M,97.7,68,14,115,78,99\n8,28,F,98.5,76,18,117,75,98\n9,41,M,98.6,70,17,119,79,96\n10,28,M,97.06,72,16,120,80,98\n
-        These are considered baseline data. If I give u a medical report/patient's health vitals. For it being true, the input values should be within the range of the baseline data. If the input values are not within the range, then it should be flagged as FALSE(if false, tell me why false- KEEP IT SHORT). If the input values are within the range, but not exactly the same, then it should be flagged as NEUTRAL."""
+        These are considered baseline data. If I give u a medical report/patient's health vitals. For it being true, the input values should be within the range of the baseline data. If the input values are not within the range, then it should be flagged as FALSE(if false, tell me why false- KEEP IT INFORMATIVE and SHORT). If the input values are within the range, but not exactly the same, then it should be flagged as NEUTRAL."""
 
     response = client.chat.completions.create(
             model="gpt-3.5-turbo",
